8075b6bc1db1497309e48d574f801d3db473cb99
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-02 00:42:31 +0200

    [UPD] Finish JS fix and comment

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,17 +1,24 @@
+// Lo script parte con il bottone
 document.getElementById("start-script-btn").addEventListener("click", function() {
-    let evenNumbers = [];
+    let oddNumbers = [];
 
+    // Cicla 6 volte per ottenere 6 numeri dall'utente
     for (let i = 0; i < 6; i++) {
+        // Chiedi all'utente di inserire un numero
         let number = prompt("Inserisci un numero:");
+        
+        // Converti la stringa inserita in un numero intero
         number = parseInt(number);
 
+        // Controlla se il numero è dispari (number % 2 === 1)
         if (number % 2 === 1) {
-            evenNumbers.push(number);
+            // Se il numero è dispari, aggiungilo all'array oddNumbers
+            oddNumbers.push(number);
         }
 
         console.log(number);
     }
 
-    console.log(evenNumbers);
-    alert("I tuoi numeri sono: " + evenNumbers.join(", "));
+    console.log(oddNumbers);
+    alert("I tuoi numeri dispari sono: " + oddNumbers.join(", "));
 });
