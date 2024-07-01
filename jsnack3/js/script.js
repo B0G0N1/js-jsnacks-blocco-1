0a6c0040e150c79ea7ef16111002afd9b41133e5
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-02 01:32:51 +0200

    [UPD] Update JS

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,9 +1,15 @@
-let number = prompt("Inserisci un numero di 4 cifre:");
-let sum = 0;
-for (let i = 0; i < number.length; i++) {
-    let digit = number.charAt(i);
-    let digitValue = parseInt(digit, 10);
-    sum += digitValue;
-}
-console.log(sum);
-alert("Il tuo numero sommato è: " + sum);
+document.getElementById("start-script-btn").addEventListener("click", function() {
+    let number = prompt("Inserisci un numero di 4 cifre:");
+    if (number.length === 4 && !isNaN(number)) {
+        let sum = 0;
+        for (let i = 0; i < number.length; i++) {
+            let digit = number.charAt(i);
+            let digitValue = parseInt(digit, 10);
+            sum += digitValue;
+        }
+        console.log(sum);
+        alert("Il tuo numero sommato è: " + sum);
+    } else {
+        alert("Per favore, inserisci un numero di 4 cifre.");
+    }
+});
