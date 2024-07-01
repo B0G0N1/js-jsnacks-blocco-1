89d9667c6f40d58ca4c5a2c94abbd5b455ec3d5a
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-01 23:44:30 +0200

    [FEAT] Finish html/css/js

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,18 +1,28 @@
-const guestsList = ["Pippo", "Pluto", "Topolino", "Tizio", "Caio", "Sempronio"];
+document.getElementById("check-partecipation-btn").addEventListener("click", function() {
+    const guestsList = ["Pippo", "Pluto", "Topolino", "Tizio", "Caio", "Sempronio"];
 
-const userName = prompt("Inserisci il tuo nome:");
+    const userName = document.getElementById("input-name").value;
 
-let canPartecipate = false;
+    let canPartecipate = false;
 
-for (let i = 0; i < guestsList.length; i++) {
-    if (guestsList[i] === userName) {
-        canPartecipate = true;
+    for (let i = 0; i < guestsList.length; i++) {
+        if (guestsList[i] === userName) {
+            canPartecipate = true;
+        }
     }
-}
 
-if (canPartecipate) {
-    alert("Sei stato invitato al party!");
-}
-else {
-    alert("Non sei stato invitato al party...");
-}
+    const partecipateBox = document.getElementById("partecipate-box");
+    const partecipateMessage = document.getElementById("partecipate-message");
+
+    if (canPartecipate) {
+        partecipateBox.classList.remove("partecipate-red");
+        partecipateBox.classList.add("partecipate-green");
+        partecipateMessage.textContent = "Sei stato invitato !!!";
+    } else {
+        partecipateBox.classList.remove("partecipate-green");
+        partecipateBox.classList.add("partecipate-red");
+        partecipateMessage.textContent = "Non sei stato invitato ...";
+    }
+
+    console.log(userName)
+});
