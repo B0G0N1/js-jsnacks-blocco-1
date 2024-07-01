c2db53107ab82ee3262b019081a176ce3f60528b
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-01 23:06:20 +0200

    [UPD] Finish js code

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,5 +1,18 @@
-const guests = ["Pippo", "Pluto", "Topolino", "Tizio", "Caio", "Sempronio"];
+const guestsList = ["Pippo", "Pluto", "Topolino", "Tizio", "Caio", "Sempronio"];
 
 const userName = prompt("Inserisci il tuo nome:");
 
 let canPartecipate = false;
+
+for (let i = 0; i < guestsList.length; i++) {
+    if (guestsList[i] === userName) {
+        canPartecipate = true;
+    }
+}
+
+if (canPartecipate) {
+    alert("Sei stato invitato al party!");
+}
+else {
+    alert("Non sei stato invitato al party...");
+}
