e80e171b7de9d582f832011e6c91dfde8da5655f
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-01 23:47:03 +0200

    [UPD] Add comments jsnack1

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,10 +1,11 @@
+// Script che parte al click del Bottone con ID check-partecipation...
 document.getElementById("check-partecipation-btn").addEventListener("click", function() {
+    // Variabili
     const guestsList = ["Pippo", "Pluto", "Topolino", "Tizio", "Caio", "Sempronio"];
-
     const userName = document.getElementById("input-name").value;
-
     let canPartecipate = false;
 
+    // Cicla attraverso la lista degli ospiti per controllare se l'utente è presente
     for (let i = 0; i < guestsList.length; i++) {
         if (guestsList[i] === userName) {
             canPartecipate = true;
@@ -14,6 +15,7 @@ document.getElementById("check-partecipation-btn").addEventListener("click", fun
     const partecipateBox = document.getElementById("partecipate-box");
     const partecipateMessage = document.getElementById("partecipate-message");
 
+    // Controlla se l'utente può partecipare e aggiorna lo stile e il messaggio del Box
     if (canPartecipate) {
         partecipateBox.classList.remove("partecipate-red");
         partecipateBox.classList.add("partecipate-green");
@@ -23,6 +25,4 @@ document.getElementById("check-partecipation-btn").addEventListener("click", fun
         partecipateBox.classList.add("partecipate-red");
         partecipateMessage.textContent = "Non sei stato invitato ...";
     }
-
-    console.log(userName)
 });
